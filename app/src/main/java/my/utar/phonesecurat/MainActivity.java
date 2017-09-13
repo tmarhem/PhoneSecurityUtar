a33de456c9ae50551571532007e5ad8a418758b6
Strynge <tmarhem@elv.telecom-lille.fr>
2017-09-14 00:45:36 +0800

    First attempt changing menu

--- app/src/main/java/my/utar/phonesecurat/MainActivity.java
+++ app/src/main/java/my/utar/phonesecurat/MainActivity.java
@@ -1,20 +1,35 @@
 package my.utar.phonesecurat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button ButtonSettings = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //Activity launch
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Test on GitHub
-        //TODO Naviguation inter activity
+
+        ButtonSettings = (Button) findViewById(R.id.ButtonSettings);
+
+        ButtonSettings.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void OnClick(View v){
+                Intent goToSettings = new Intent(MainActivity.this , SettingsActivity.class);
+                startActivity(goToSettings);
+            }
+
+        });
+        //TODO  //DOING// Naviguation interactivity
         //TODO remplissage basic other activity
         //TODO retreive swiping gesture
 
     }
+
 }
 
