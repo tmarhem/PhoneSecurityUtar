2999f57066c0490088f408bcef3e00e2c49d64c8
Unknown <tmarhem@elv.telecom-lille.fr>
2018-01-11 01:11:48 +0800

    Link
    https://github.com/Shookit/android-ml-weka

--- app/src/main/java/my/utar/phonesecurat/BaseProfilingActivity.java
+++ app/src/main/java/my/utar/phonesecurat/BaseProfilingActivity.java
@@ -6,7 +6,6 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.GestureDetector;
@@ -16,9 +15,6 @@ import android.view.VelocityTracker;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import com.google.gson.Gson;
-
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.ArrayList;
